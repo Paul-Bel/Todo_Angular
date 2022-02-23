8445df59c18afeee290e0f738404ef48c4334bf6
Paul-Bel <pn.beloborodov.msk@gmail.com>
2022-02-23 23:45:40 +0300

    added small style

--- src/app/components/td-input/todo-input.component.ts
+++ src/app/components/td-input/todo-input.component.ts
@@ -15,6 +15,7 @@ export class TodoInputComponent implements OnInit {
   ngOnInit(): void {
   }
   submitTodo(){
+    if(this.todoTitle.trim())
     this.todoTitleSubmit.emit(this.todoTitle)
     this.todoTitle = ''
   }
